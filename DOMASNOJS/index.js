7dfc95275ad2eacfb70169da7fce1e70ce7bb50d
Aleksandra-Basarovska <aleksandrabasarovska@yahoo.com>
2023-03-06 13:19:45 +0100

    domasnoJS

---
+++ DOMASNOJS/index.js
@@ -0,0 +1,29 @@
+let a=1;//number
+let b=0;//number
+let c="1";//string
+let d="0";//string
+let e="ten";//string
+let f=true;//boolean
+let g=false;//boolean
+let x=null;//null
+let y=undefined;//null
+let z="";//string
+let j='';//string
+
+
+
+
+
+
+
+
+//domasno pod 2
+let birthDay=5;//05.05.1984
+let fuulName=20;
+let year=65;//1984=84-19
+let years=1+9+8+4;
+            let test=birthDay>fuulName;
+            let test1=year%years;
+            let test2="Iam the";
+if(test==true && test1<3){console.log(test2="light")}
+else(test==false && test1>3){console.log(test2="darkness")}
